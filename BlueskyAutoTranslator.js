2afd1d1c3d5e05c1bdb9647fa3d6a58dce722ea3
Lamron <lamrongol@gmail.com>
2024-04-13 19:16:59 +0900

    Corresponds Portuguese language

--- BlueskyAutoTranslator.js
+++ BlueskyAutoTranslator.js
@@ -1,16 +1,14 @@
 // ==UserScript==
-// @name         Blueskyの英語とドイツ語を自動的に翻訳
+// @name         Blueskyの外国語を自動的に翻訳
 // @namespace    @lamrongol
-// @version      0.1.7
-// @description  Blueskyの日本語文字が含まれてないポストを自動で翻訳。ただし英語とドイツ語以外は翻訳できない。
-// @author       Loran🍞
+// @version      0.1.8
+// @description  Blueskyの日本語文字が含まれてないポストを自動で翻訳(英語・ドイツ語・ポルトガル語のみ)
+// @author       Lamron🍞
 // @match        https://bsky.app/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bsky.app
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
 // @license MIT
 // @grant        none
-// @downloadURL https://update.greasyfork.org/scripts/472873/Bluesky%E3%81%AE%E8%8B%B1%E8%AA%9E%E3%81%A8%E3%83%89%E3%82%A4%E3%83%84%E8%AA%9E%E3%82%92%E8%87%AA%E5%8B%95%E7%9A%84%E3%81%AB%E7%BF%BB%E8%A8%B3.user.js
-// @updateURL https://update.greasyfork.org/scripts/472873/Bluesky%E3%81%AE%E8%8B%B1%E8%AA%9E%E3%81%A8%E3%83%89%E3%82%A4%E3%83%84%E8%AA%9E%E3%82%92%E8%87%AA%E5%8B%95%E7%9A%84%E3%81%AB%E7%BF%BB%E8%A8%B3.meta.js
 // ==/UserScript==
 
 (function() {
@@ -22,6 +20,7 @@
 
     const jaRe = /[\p{Script_Extensions=Hiragana}\p{Script_Extensions=Katakana}\p{Script_Extensions=Han}]/u
     const geRe = /[äöüÄÖÜß]/u
+    const ptRe = /[áéíóúâêôãõç]/u//ref https://amadora.co.uk/aprender/alfabeto/
 
     const postTextCSS = ".css-175oi2r > .css-175oi2r.r-1awozwy.r-18u37iz.r-1w6e6rj > .css-146c3p1";
     //const quotePostTextCSS;
@@ -46,7 +45,10 @@
         const originalText = elem.textContent;
         if(jaRe.test(originalText)) continue;
 
-        const sourceLang = geRe.test(originalText) ? "de" : "en";
+        let sourceLang = "en";
+        if(geRe.test(originalText)) sourceLang = "de";
+        else if(ptRe.test(originalText)) sourceLang = "pt";
+
         const base_url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=`;
 
         const encodeText = encodeURIComponent(originalText);
